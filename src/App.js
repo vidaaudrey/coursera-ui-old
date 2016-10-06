92c1dfd86a799204724a641c5180f25e82dc1dbb
Audrey Li <ali@coursera.org>
2016-10-05 17:01:47 -0700

    chore: setup eslint and flow config
    1. use airbnb’s config, add basic lint and lint fix
    2. add readme doc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
+// @flow
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = ({name = 'Coursera'}) => {
+  const appName: string = `${name} hello flow `;
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>{appName}</h2>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+App.propTypes = {
+  name: React.PropTypes.string,
+};
 
 export default App;
